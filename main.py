02c59df7e37c23506f61cd7f30ef0755ea7fffef
Brian-Camacho15 <briancamacho901@gmail.com>
2025-01-19 22:01:06 -0800

    Changed the welcome message

--- main.py
+++ main.py
@@ -8,3 +8,15 @@ def welcome_message(name:str) -> str:
 
 message = welcome_message("bcamac09@calpoly.edu")
 print(message)
+
+
+def smallest(n: float, m: float) -> float:
+   if n < m:
+      return n
+   else:
+      return m
+
+
+first = smallest(3, 2)  # 2
+second = smallest(2, 2)  # 2 NO becuase they have the same value
+print(second)
